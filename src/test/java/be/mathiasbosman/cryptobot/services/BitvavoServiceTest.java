74ccc579fe34cfe307c00e704ea3cf9b5bdf051e
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2021-06-25 15:36:15 +0200

    Stage 1

--- src/test/java/be/mathiasbosman/cryptobot/services/BitvavoServiceTest.java
+++ src/test/java/be/mathiasbosman/cryptobot/services/BitvavoServiceTest.java
@@ -2,6 +2,7 @@ package be.mathiasbosman.cryptobot.services;
 
 import org.junit.jupiter.api.Test;
 
+//todo finish this
 class BitvavoServiceTest {
 
   @Test
@@ -13,13 +14,5 @@ class BitvavoServiceTest {
   @Test
   void estimateSellingReturn() {
     // assertThat(estimateSellingReturn(995.36759319, 0.0049940, 0.0025)).isEqualTo(4.95);
-    System.out.println(test(102.749696, 0.52982, 0.0025));
-  }
-
-  private double test(double amount, double price, double feeMultiplier) {
-    double cost = amount * price;
-    double fee = Math.ceil(cost * feeMultiplier * 100) / 100.0;
-    double totalCost = cost - fee;
-    return Math.round(totalCost * 100) / 100.0;
   }
 }
